67b9e5fc64c64f57c3428d0ed22a321d920a4502
Mario Elorza <mario.elorza@gmail.com>
2020-01-08 21:34:50 -0300

    Initial Commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Mario Elorza on 08-01-20.
+//  Copyright © 2020 Mario Elorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
