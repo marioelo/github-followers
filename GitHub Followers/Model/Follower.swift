ccf2f2af4086e6ba7f824a79a49eb4c107425ad7
Mario Elorza <mario.elorza@gmail.com>
2020-01-15 23:27:09 -0300

    network calls - add models

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Mario Elorza on 15-01-20.
+//  Copyright © 2020 Mario Elorza. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
