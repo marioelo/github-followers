8d6b465775ca152971b95a21877fd95e5b167651
Mario Elorza <mario.elorza@gmail.com>
2020-01-18 22:10:47 -0300

    add loading view

--- GitHub Followers/Screens/FollowerListVC.swift
+++ GitHub Followers/Screens/FollowerListVC.swift
@@ -52,8 +52,10 @@ class FollowerListVC: UIViewController {
     
 //    MARK: Get followers
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
