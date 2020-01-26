437006d799ac9ebd07ca45e586eb5d95adc6ab39
Mario Elorza <mario.elorza@gmail.com>
2020-01-26 13:11:08 -0300

    add user info header

---
+++ GitHub Followers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHub Followers
+//
+//  Created by Mario Elorza on 25-01-20.
+//  Copyright © 2020 Mario Elorza. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
