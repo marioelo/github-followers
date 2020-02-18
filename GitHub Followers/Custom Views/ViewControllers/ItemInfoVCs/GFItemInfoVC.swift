16ceb7157c745c29fd095c7d0de30780abf58e97
Mario Elorza <mario.elorza@cencosudscotiabank.cl>
2020-02-17 23:10:26 -0300

    [CHG] presentSafariVC into UIViewController Extension

--- GitHub Followers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ GitHub Followers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -17,7 +17,7 @@ class GFItemInfoVC: UIViewController {
     
     var user: User!
     
-    var delegate: UserInfoVCDelegate!
+    weak var delegate: UserInfoVCDelegate!
     
     init(user: User) {
         super.init(nibName: nil, bundle: nil)
