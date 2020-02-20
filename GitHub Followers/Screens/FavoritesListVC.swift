4b9d0e184cda123d0d83e39cc3db5250d26fad64
Mario Elorza <mario.elorza@cencosudscotiabank.cl>
2020-02-19 22:11:39 -0300

    [ADD] addButtonTapped function to FollowerListVC

--- GitHub Followers/Screens/FavoritesListVC.swift
+++ GitHub Followers/Screens/FavoritesListVC.swift
@@ -13,5 +13,15 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+                break
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
